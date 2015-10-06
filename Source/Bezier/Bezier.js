c69bac49700a9bddfb651de91441f00b41471e58
Robin Seibold <robinbobseibold@gmail.com>
2015-10-06 13:44:47 +0200

    Add De Casteljau function

--- Source/Bezier/Bezier.js
+++ Source/Bezier/Bezier.js
@@ -4,3 +4,29 @@ var controlPoints = [];
 function addControlPoint(point) {
 	controlPoints.push(point);
 }
+
+function firstControlPoint() {
+    return controlPoints[0];
+}
+
+function calculateBezierCurveValueWithDeCasteljau(t) {
+    var degree = controlPoints.length - 1;
+    
+    if (degree < 1) {
+        return;
+    }
+    
+    var b = controlPoints;
+    for (j = 1; j <= degree; j++) {
+        var bNew = [];
+        
+        for (i = 0; i <= (degree - j); i++) {
+            bNew.push({x: ((1 - t) * b[i].x + t * b[i + 1].x),
+                       y: ((1 - t) * b[i].y + t * b[i + 1].y)});
+        }
+        
+        b = bNew;
+    }
+    
+    return b[0];
+}
